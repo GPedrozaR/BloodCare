24d703b43a15e593bd64879a8f45833f9e15eace
Gabriel Pedroza <gpedrozarodrigues@gmail.com>
2024-05-16 07:28:40 -0300

    feat(Domain): Implementing entity properties

---
+++ BloodCare.Domain/Entities/Donation.cs
@@ -0,0 +1,18 @@
+﻿namespace BloodCare.Domain.Entities
+{
+    internal class Donation
+    {
+        public Donation(int donorId, DateTime donationDate, int milliliters, Donor donor)
+        {
+            DonorId = donorId;
+            DonationDate = donationDate;
+            Milliliters = milliliters;
+            Donor = donor;
+        }
+
+        public int DonorId { get; private set; }
+        public DateTime DonationDate { get; private set; }
+        public int Milliliters { get; private set; }
+        public Donor Donor { get; private set; }
+    }
+}
