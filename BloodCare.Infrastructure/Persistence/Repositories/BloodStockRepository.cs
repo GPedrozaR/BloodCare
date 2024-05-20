ffc751eea75c6e2403e16039819f24e3e377f14d
Gabriel Pedroza <gpedrozarodrigues@gmail.com>
2024-05-20 20:47:00 -0300

    feat(Infrastructure): Implement repositories

---
+++ BloodCare.Infrastructure/Persistence/Repositories/BloodStockRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace BloodCare.Infrastructure.Persistence.Repositories
+{
+    public class BloodStockRepository : Repository<BloodStock>, IBloodStockRepository
+    {
+        public BloodStockRepository(IMongoDatabase database)
+            : base(database, "BloodStocks") { }
+    }
+}
---
+++ BloodCare.Infrastructure/Persistence/Repositories/DonationRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace BloodCare.Infrastructure.Persistence.Repositories
+{
+    public class DonationRepository : Repository<Donation>, IDonationRepository
+    {
+        public DonationRepository(IMongoDatabase database)
+            : base(database, "Donations") { }
+    }
+}
